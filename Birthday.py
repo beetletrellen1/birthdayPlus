37f7eb2f5c30cf4bc5be42428813c5d50254929a
Brian <beetletrellen@gmail.com>
2023-03-25 17:09:24 -0400

    added in graphs and weighted birthdays

--- Birthday.py
+++ Birthday.py
@@ -1,19 +1,26 @@
 from calendar import day_abbr, month
+import random
 import numpy as np
+import pandas as pd
 from collections import Counter
 from datetime import date
+import plotly.express as px
 
 class Birthday:
 
     def __init__(self, seed, num_trials, min_people, max_people, n_share_bday, 
-                    possible_days = 365):
+                    possible_days = 366):
         self.seed = int(seed)
         self.num_trials = num_trials
         self.min_people = min_people
         self.max_people = max_people
         self.shared_bday = n_share_bday
         self.num_possible_bdays = possible_days
-
+        self.trial_list =[]
+        self.has_coincidence = []
+        self.probability = []
+        self.df = None
+        self.fig = None
 
     def generate_random_birthday(self):
         return np.random.randint(1, self.num_possible_bdays)
@@ -25,19 +32,17 @@ class Birthday:
         unique_birthdays = Counter(birthdays)
         return any(same_bday >= x for same_bday in unique_birthdays.values())
 
-    def estimate_p_coincidence(self, people=None):
-        np.random.seed(self.seed)
+    def estimate_p_coincidence(self, idx, people=None):
         if people == None:
             people = self.max_people
 
-        num_coincidence = 0
-        for _ in range(self.num_trials):
-            birthdays = self.generate_k_birthdays(people)
-            has_coincidence = self.x_people_share_bday(birthdays, x=self.shared_bday)
-            if has_coincidence:
-                num_coincidence += 1
-        
-        return num_coincidence / self.num_trials
+        birthdays = self.generate_k_birthdays(people)
+        has_coincidence = self.x_people_share_bday(birthdays, x=self.shared_bday)
+        self.has_coincidence.append(has_coincidence)
+        self.calc_p()
+
+    def calc_p(self):
+        self.probability.append(sum(self.has_coincidence) / len(self.has_coincidence))
 
 
 class MyBirthday(Birthday):
@@ -63,17 +68,49 @@ class MyBirthday(Birthday):
 
     def estimate_p_share_my_birthday(self, threshold = 0.1):
         num_people = 1
-        p_share = [0]
         
-        while p_share[-1] < threshold:
+        while self.probability[-1] < threshold:
             num_people += 1
-            num_coincidence = 0
             for _ in range(self.num_trials):
                 birthdays = self.generate_k_birthdays(num_people)
-                has_coincidence = self.my_birthday_coincidence(birthdays)
-                if has_coincidence:
-                    num_coincidence += 1
+                self.has_coincidence.append(self.my_birthday_coincidence(birthdays))
+                self.calc_p()
 
-            p_coincidence = num_coincidence / self.num_trials
-            p_share.append(p_coincidence)
-        return p_share
+
+class WeightedBirthdays(Birthday):
+
+    def create_df(self):
+        self.df = pd.read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/births/US_births_1994-2003_CDC_NCHS.csv")
+
+    def group_bday_by_day(self):
+        self.avg_bday_df = self.df.groupby(['month', 'date_of_month']).agg({'births': np.mean}).reset_index()
+
+    def create_heat_map(self):
+        self.month_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        self.heat_df = self.avg_bday_df.pivot(index='month', columns='date_of_month')['births']
+        fig = px.imshow(self.heat_df, labels=dict(x="Day of Month", y="Month"),
+                                x=[str(i+1) for i in range(31)],
+                                y=self.month_labels,
+                                color_continuous_scale='RdBu_r', 
+                                title="Average Birthdays by Day")
+        fig.update_xaxes(side="top")
+        return fig
+    
+    def create_bday_weight(self):
+        total_births = np.sum(self.avg_bday_df['births'])
+        self.birth_weight = [birth / total_births for birth in self.avg_bday_df['births']]
+
+    def generate_random_birthday(self):
+        return random.choices(range(1, 367), weights=self.birth_weight, k=1)[0]
+
+if __name__ == "__main__":
+    bday = WeightedBirthdays(seed=1, num_trials=100, min_people=10, max_people=40, n_share_bday=3, 
+                    possible_days = 366)
+    bday.create_df()
+    bday.group_bday_by_day()
+    bday.create_bday_weight()
+    for idx, _ in enumerate(range(100)):
+        bday.estimate_p_coincidence(idx=idx)
+        bday.trial_list.append(idx)
+        p_coincidence = bday.probability[-1]
+    print(p_coincidence)
--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import random
+import numpy as np
 
 from dash import Dash, dcc, html, ctx
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import plotly.express as px
 
-from Birthday import Birthday
+from Birthday import Birthday, WeightedBirthdays
 
 NUM_PEOPLE = 23
 MIN_NUM_PEOPLE = 2
 MAX_NUM_PEOPLE = 30
 ks = range(MIN_NUM_PEOPLE, MAX_NUM_PEOPLE + 1)
 NUM_POSSIBLE_BIRTHDAYS = 365
-NUM_TRIALS = 1000
+NUM_TRIALS = 100
 
 app = Dash(__name__)
 
@@ -18,6 +21,7 @@ app.layout = html.Div([
         html.H1(children="Birthday Plus Problem"),
         html.H2(children="ISYE 6644"),
         html.H2(children="Authors: Stacey Helcher, Brian Collins"),
+        html.Br(),
         dcc.Input(id="input_seed", 
                     type="number", 
                     placeholder="Add Seed Value", 
@@ -26,39 +30,115 @@ app.layout = html.Div([
                     type="number", 
                     placeholder="Number Shared Birthdays", 
                     style={'marginRight':'20px'}),
+        dcc.Input(id="num-people",
+                    type="number",
+                    placeholder="Number of People",
+                    style={'marginRight':'20px'}),
+        html.Br(),
         html.Div(id="seed_output"),
+        html.Div(id="shared-bday"),
+        html.Div(id="num-people-output"),
+        html.Br(),
         html.Button('Run', id='run-button', n_clicks=0),
-        html.Div(id='container-button-timestamp')
+        html.Div(id='container-button-timestamp'),
+        dcc.Graph(id='graph'),
+        html.Br(),
+        html.Br(),
+        html.Button('Run Weights', id='run-weights-button', n_clicks=0),
+        html.Div(id='container-button'),
+        dcc.Graph(id='bday-heatmap'),
+        dcc.Graph(id='graph-weights')
         ]
 )
 
 
 @app.callback(
     Output("seed_output", "children"),
-    inputs = [Input("input_seed", "value"),
-              Input("num-share-bday", "value")],
+    Input("input_seed", "value"),
 )
-def update_output(input_seed, n_shared):
-    return u'Random Seed Set: {}'.format(input_seed)
+def update_output(input_seed):
+    return u'''Random Seed Set: {}'''.format(input_seed)
+@app.callback(
+    Output("shared-bday", "children"),
+    Input("num-share-bday", "value"),
+)
+def update_output(n_shared):
+    return u'''The Number of People to share a birthday: {}'''.format(n_shared)
+
+@app.callback(
+    Output("num-people-output", "children"),
+    Input("num-people", "value"),
+)
+def update_output(num_people):
+    return u'''Number of Participants {}'''.format(num_people)
 
 @app.callback(
     Output('container-button-timestamp', 'children'),
+    Output('graph', 'figure'),
     Input('run-button', 'n_clicks'),
     Input('input_seed', 'value'),
-    Input('num-share-bday', 'value')
+    Input('num-share-bday', 'value'),
+    Input('num-people', 'value')
 )
-def run(run_button, seed, n_share_bday):
+def run(run_button, seed, n_share_bday, num_people):
     msg = "None of the buttons have been clicked yet"
+    fig = go.Figure()
     if n_share_bday == None:
         n_share_bday = 2
     if "run-button" == ctx.triggered_id:
-        bday = Birthday(seed, NUM_TRIALS, MIN_NUM_PEOPLE, MAX_NUM_PEOPLE, n_share_bday)
-        p_coincidence = bday.estimate_p_coincidence()
+        bday = Birthday(seed, NUM_TRIALS, MIN_NUM_PEOPLE, num_people, n_share_bday)
+        np.random.seed(bday.seed)
+        for idx, _ in enumerate(range(bday.num_trials)):
+            bday.estimate_p_coincidence(idx=idx)
+            bday.trial_list.append(idx)
+            p_coincidence = bday.probability[-1]
+        fig = px.scatter(x=bday.trial_list, y=bday.probability, 
+                            labels={"x": "Trial Number", "y": "Probability"},
+                            title=f'Probability of Shared Birthday with {NUM_TRIALS} trials')
+            
         msg = f"""
                 Probability {n_share_bday} people sharing a birthday 
-                from a of pool of {MAX_NUM_PEOPLE} is {p_coincidence}
+                from a of pool of {num_people} is {p_coincidence}
                """
-    return html.Div(msg)
+    return html.Div(msg), fig
+
+
+@app.callback(
+    Output('container-button', 'children'),
+    Output('bday-heatmap', 'figure'),
+    Output('graph-weights', 'figure'),
+    Input('run-weights-button', 'n_clicks'),
+    Input('input_seed', 'value'),
+    Input('num-share-bday', 'value'),
+    Input('num-people', 'value')
+)
+def run(run_button, seed, n_share_bday, num_people):
+    msg = "None of the buttons have been clicked yet"
+    fig_heatmap = go.Figure()
+    fig_weights = go.Figure()
+    if n_share_bday == None:
+        n_share_bday = 2
+    if "run-weights-button" == ctx.triggered_id:
+        bday = WeightedBirthdays(seed, NUM_TRIALS, MIN_NUM_PEOPLE, num_people, n_share_bday)
+        bday.create_df()
+        bday.group_bday_by_day()
+        bday.create_bday_weight()
+        fig_heatmap = bday.create_heat_map()
+        np.random.seed(bday.seed)
+        for idx, _ in enumerate(range(bday.num_trials)):
+            bday.estimate_p_coincidence(idx=idx)
+            bday.trial_list.append(idx)
+            p_coincidence = bday.probability[-1]
+        fig_weights = px.scatter(x=bday.trial_list, y=bday.probability, 
+                            labels={"x": "Trial Number", "y": "Probability"},
+                            title=f'Probability of Shared Birthday with {NUM_TRIALS} trials')
+            
+        msg = f"""
+                Probability {n_share_bday} people sharing a birthday 
+                from a of pool of {num_people} is {p_coincidence}
+               """
+    return html.Div(msg), fig_heatmap, fig_weights
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
